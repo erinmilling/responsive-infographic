7c8405bde40a1028db5fb9343d65aa6e20e4c1a7
erinmilling <mill0814@algonquinlive.com>
2016-04-22 09:44:07 -0400

    added waypoints, click title

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@ var $seedling = $('.seedling');
 var $sprout = $('.sprout');
 var $plant = $('.plant');
 var $bigplant = $('.bigplant');
+
+$seed.waypoint(function() {
+  $seed.addClass('js-seed-animate');
+}, {offset: '50%' });
+
+$seedling.waypoint(function() {
+  $seedling.addClass('js-seedling-animate');
+}, {offset: '50%' });
+
+$sprout.waypoint(function() {
+  $sprout.addClass('js-sprout-animate');
+}, {offset: '50%' });
+
+$plant.waypoint(function() {
+  $plant.addClass('js-plant-animate');
+}, {offset: '50%' });
+
+$bigplant.waypoint(function() {
+  $bigplant.addClass('js-bigplant-animate');
+}, {offset: '50%' });
+
+$('.click-title').on('click', function () {
+  $('.thetitle').toggleClass('show');
+});
